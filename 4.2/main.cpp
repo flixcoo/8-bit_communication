fa7a30e84953d0a23ef73d910c19fdfd318b9639
fkrichn1 <felix.kirchner@stud.htwk-leipzig.de>
2023-12-12 21:01:16 +0100

    Added Top Comment

--- 4.2/main.cpp
+++ 4.2/main.cpp
@@ -1,3 +1,5 @@
+//PLACEHOLDER CODE
+
 #include <iostream>
 #include <b15f/b15f.h>
 #include <vector>
@@ -146,49 +148,3 @@ char binToChar(const std::string &in)
     }
     return temp;
 }
-
-//unused
-/*string setStringtoASCII(string str)
-{
-    int size = int(str.size());
-
-    // If given string is not a valid string
-    if (size % 8 != 0)
-    {
-        return "Not Possible!";
-    }
-
-    // To store final answer
-    string asciiStr = "";
-
-    // Loop to iterate through string
-    for (int i = 0; i < size; i += 8)
-    {
-        int decimal_value = binaryToDecimal((str.substr(i, 8)));
-
-        // Apprend the ASCII character equivalent to current value
-        asciiStr += char(decimal_value);
-    }
-    return asciiStr;
-}
-
-int binaryToDecimal(string binary)
-{
-    // Stores the decimal value
-    int dec_value = 0;
-    // Initializing base value to 1
-    int base = 1;
-
-    int len = binary.length();
-    for (int i = len - 1; i >= 0; i--)
-    {
-
-        // If the current bit is 1
-        if (binary[i] == '1')
-            dec_value += base;
-        base = base * 2;
-    }
-    // Return answer
-    return dec_value;
-}*/
-
