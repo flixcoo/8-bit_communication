d26f80c2b34637628048cf028230f9a1b2a88542
fkrichn1 <felix.kirchner@stud.htwk-leipzig.de>
2024-01-24 18:12:36 +0100

    🚀 (main.hpp): add assignPC() function to set the PC_ID based on the initial signal   📝 (main.hpp): add checkIfStartSign() function to check if the received signal is the start sign   📝 (main.hpp): add checkIfStopSign() function to check if the received signal is the stop sign 📝 (main.hpp): remove checkEscape() function as it is not needed   📝 (main.hpp): add checkIfSameSign() function to check if the received signal is the same sign   🚀 (main.cpp): add assignPC() function to set the PC_ID based on the initial signal   📝 (main.cpp): remove unused main function code and comments   📝 (main.cpp): remove sendEscape() function as it is not used   📝 (main.cpp): remove checkEscape() function as it is not used   ✨ (main.cpp): add revieveChar() function to handle receiving characters   📝 (main.cpp): remove sendChar() function as it is not used   📝 (main.cpp): remove binaryToChar() function as it is not used♻️ (main.hpp): refactor function names and add comments for clarity and documentation ✨ (main.hpp): introduce functions checkIfStartSign and checkIfStopSign to check for start and stop signals respectively 📝 (main.cpp): update comments and add missing semicolon ⬆️ (main.cpp): update main function to use updated assignPC function instead of duplicated code ♻️ (main.cpp): refactor code to improve readability and add comments to explain functionality ♻️ (main.cpp): refactor sendChar function to improve semantics and remove commented out code ♻️ (main.cpp): refactor binaryToChar function to improve semantics and add comments

--- 4.1/main.hpp
+++ 4.1/main.hpp
@@ -4,8 +4,10 @@
 
 using namespace std;
 
+void assignPC(B15F&);
 void sendEscape(B15F&);
 void sendChar(char, B15F&);
-bool checkEscape(B15F&);
+bool checkIfStartSign(B15F&);
+bool checkIfStopSign(B15F&);
 void revieceChar(B15F&);
 char binaryToChar(const string);
--- 4.1_new/main.cpp
+++ 4.1_new/main.cpp
@@ -1,10 +1,31 @@
 #include "main.hpp"
 
-int main()
-{
+int PC_ID = 5;
+final int aFinalVar = 0;
+final uint8_t STOP_SIGN = 0b1111;
+final uint8_t START_SIGN = 0b1000;
+final uint8_t SAME_SIGN = 0b1001;
+
+
+// int main(){
+//     B15F &drv = B15F::getInstance();
+//     while(1){
+//         drv.setRegister(&DDRA, 0x00);
+//         //string binary = bitset<3>(drv.getRegister(&PINA)).to_string();
+//         //cout << "Binary: " << binary << endl;
+//         uint8_t data = (drv.getRegister(&PINA) & 0x03);
+//         std::cout << "bekommen: " << std::bitset<8>(data) << " bzw.: x" << (unsigned int)data << std::endl;
+//         drv.delay_ms(200);
+//     }
+//}
+
+int main(){
     B15F &drv = B15F::getInstance();
+    drv.setRegister(&DDRA, 0x00);
+    assignPC(drv);
+
     bool running = true;
-    drv.setRegister(&DDRA, 0x0F);
+
     while (running)
     {
         int decision;
@@ -35,88 +56,110 @@ int main()
         cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
     cout << endl << "[System]: Programm beendet" << endl << endl;
-}
-
-void sendEscape(B15F &drv)
-{
     
-    drv.setRegister(&DDRA, 0x07);
-    cout << "[System]: ESC wird gesendet..." << endl;
-    // ESC: 101-010-101 or 5-2-5
-    drv.setRegister(&PORTA, 0b101);
-    drv.delay_ms(500);
-    drv.setRegister(&PORTA, 0b010);
-    drv.delay_ms(500);
-    drv.setRegister(&PORTA, 0b101);
-    cout << "[System]: ESC gesendet" << endl;
-    drv.delay_ms(500);
 }
 
-bool checkEscape(B15F &drv)
-{
-    drv.delay_ms(500);
-    int input = (int)drv.getRegister(&PINA);
-    if (input == 5)
-    {
-        drv.delay_ms(500);
-        input = (int)drv.getRegister(&PINA);
-        if (input == 2)
-        {
-            drv.delay_ms(500);
-            input = (int)drv.getRegister(&PINA);
-            if (input == 5)
-            {
-                cout << "[System]: ESC empfangen" << endl;
-                return true;
-            }
-        }
+void assignPC(B15F &drv){
+    uint8_t initialSignal = drv.getRegister(&PINA);
+    cout << "initialSignal: " << (unsigned int)initialSignal << endl;
+    
+    if(initialSignal == 0){
+        drv.setRegister(&DDRA, 0x0F);//PC-1
+        drv.setRegister(&PORTA, 0b0001);
+        PC_ID = 1;
+        PC_ID = 3;
+        cout << "[Sytem]: PC auf PC-" << PC_ID << " gesetzt." << endl;
     }
-    cout << "[System]: Kein ESC bekommen" << endl;
+    else{
+        drv.setRegister(&DDRA, 0xF0);
+        drv.setRegister(&PORTA, 0b0000);
+        PC_ID = 2;
+        cout << "[Sytem]: PC auf PC-" << PC_ID << " gesetzt." << endl;
+    }
+}
+//Steuerzeichen
+//1000 - Es wird gesendet
+//1001 - Das letzte Zeichen doppelt
+//1111 - Übertragung beendet
+
+
+
+// void sendEscape(B15F &drv)
+// {
+    
+//     drv.setRegister(&DDRA, 0x07);
+//     cout << "[System]: ESC wird gesendet..." << endl;
+//     // ESC: 101-010-101 or 5-2-5
+//     drv.setRegister(&PORTA, 0b101);
+//     drv.delay_ms(500);
+//     drv.setRegister(&PORTA, 0b010);
+//     drv.delay_ms(500);
+//     drv.setRegister(&PORTA, 0b101);
+//     cout << "[System]: ESC gesendet" << endl;
+//     drv.delay_ms(500);
+// }
+
+bool checkIfStartSign(B15F &drv){
+    if(drv.getRegister(&PINA) == START_SIGN)
+        return true;
+    return false;
+}
+
+bool checkIfStopSign(B15F &drv){
+    if(drv.getRegister(&PINA) == STOP_SIGN)
+        return true;
+    return false;
+}
+
+bool checkIfSameSign(B15F &drv){
+    if(drv.getRegister(&PINA) == SAME_SIGN)
+        return true;
     return false;
 }
 
 void revieceChar(B15F &drv)
 {
-    drv.setRegister(&DDRA, 0x00);
     bool active = false;
+    while (!checkIfStartSign(drv))
+        cout << "checkIfSending = false" << endl;
+    cout << "checkIfSending = false" << endl;
 
-    while (!active)
-        active = checkEscape(drv);
+    vector<uint8_t> buffer;
+    uint8_t prevToken = START_SIGN;
+    while(!checkIfStopSign){
+        if(!(drv.getRegister(&PINA) == prevToken)) //Aktuelles Zeichen ist ein neues Zeichen
+            if(drv.getRegister(&PINA) == SAME_SIGN) //Aktuelles Zeichen ist die SAME_SIGN Konstante
+            {
+                buffer.push_back(prevToken);
+            }
+            else{
+                prevToken = drv.getRegister(&PINA);
+                buffer.push_back(prevToken);
+            }
 
-    vector<int> binary;
-    for (int i = 0; i < 3; i++)
-    {
-        drv.delay_ms(500);
-        binary.push_back((int)drv.getRegister(&PINA));
     }
 
-    string s = "";
-    for (int i = 0; i < 3; i++)
-        s += std::bitset<3>(binary.at(i)).to_string();
-
-    cout << "[System]: " << s << " empfangen" << endl;
-    cout << "[System]: " << binaryToChar(s) << " (ASCII-Umwandlung)" << endl;
 }
 
-void sendChar(const char c, B15F &drv)
-{
-    drv.setRegister(&DDRA, 0x07);
-    string binary = bitset<9>((int)c).to_string();
-    cout << "Wert: " << c << endl
-         << "Binary: " << binary << endl; // Debug
+// void sendChar(const char c, B15F &drv)
+// {
+//     drv.setRegister(&DDRA, 0x07);
+//     string binary = bitset<9>((int)c).to_string();
+//     cout << "Wert: " << c << endl
+//          << "Binary: " << binary << endl; // Debug
 
-    for (long unsigned int i = 0; i < binary.length(); i += 3)
-    {
-        drv.setRegister(&PORTA, stoi(binary.substr(i, 3)));
-        drv.delay_ms(500);
-    }
-}
+//     for (long unsigned int i = 0; i < binary.length(); i += 3)
+//     {
+//         drv.setRegister(&PORTA, stoi(binary.substr(i, 3)));
+//         drv.delay_ms(500);
+//     }
+// }
 
-char binaryToChar(const string binary)
-{
-	char c = 0;
-	for (long unsigned int i = 0; i < binary.length(); i++)
-		if ('1' == binary.at(i))
-			c += (int)pow(2, 8 - i);
-	return c;
-}
+// char binaryToChar(const string binary)
+// {
+// 	char c = 0;
+// 	for (long unsigned int i = 0; i < binary.length(); i++)
+// 		if ('1' == binary.at(i))
+// 			c += (int)pow(2, 8 - i);
+// 	return c;
+// }
